58968fc23dc7a24c2f5e73dc75c259d0be3d871e
Sheifer Kepler <sheifer.kda@gmail.com>
2023-01-30 13:00:28 -0300

    feat: model users

---
+++ app/backend/src/database/models/Users.ts
@@ -0,0 +1,42 @@
+import { DataTypes, Model } from 'sequelize';
+import db from '.';
+
+class Users extends Model {
+  declare id: number;
+  declare user: string;
+  declare role: string;
+  declare email: string;
+  declare password: string;
+}
+
+Users.init({
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: DataTypes.INTEGER,
+  },
+  user: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  role: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+}, {
+  sequelize: db,
+  tableName: 'users',
+  underscored: true,
+  timestamps: false,
+});
+
+export default Users;
