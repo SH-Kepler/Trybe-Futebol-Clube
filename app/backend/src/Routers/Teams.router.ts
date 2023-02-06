ba4cbcc3796ac3cb7837aa4fa6c53f3ef785a3d4
Sheifer Kepler <sheifer.kda@gmail.com>
2023-02-06 12:57:36 -0300

    feat: router get team by id

--- app/backend/src/Routers/Teams.router.ts
+++ app/backend/src/Routers/Teams.router.ts
@@ -4,5 +4,6 @@ import TeamsController from '../controllers/Teams.controller';
 const router = Router();
 
 router.get('/', TeamsController.getAllTeams);
+router.get('/:id', TeamsController.getTeam);
 
 export default router;
