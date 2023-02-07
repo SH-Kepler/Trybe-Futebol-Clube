ae0f8ef5d04867837a94d3757c68b02c696628cc
Sheifer Kepler <sheifer.kda@gmail.com>
2023-02-07 16:02:46 -0300

    feat: filter matches in progress

--- app/backend/src/controllers/Matches.controller.ts
+++ app/backend/src/controllers/Matches.controller.ts
@@ -3,7 +3,10 @@ import MatchesService from '../services/Matches.service';
 
 export default class MatchesController {
   static async getAllMatches(req: Request, res: Response) {
-    const { status, message } = await MatchesService.getAllMatches();
+    const { inProgress } = req.query;
+
+    const { status, message } = await MatchesService
+      .getAllMatches(inProgress as string | undefined);
 
     return res.status(status).json(message);
   }
--- app/backend/src/services/Matches.service.ts
+++ app/backend/src/services/Matches.service.ts
@@ -2,8 +2,9 @@ import Teams from '../database/models/Teams';
 import Matches from '../database/models/Matches';
 
 export default class MatchesService {
-  static async getAllMatches() {
+  static async getAllMatches(inProgress: string | undefined) {
     const matches = await Matches.findAll({
+      where: inProgress === 'true' ? { inProgress: true } : { inProgress: false },
       include: [
         { model: Teams, as: 'homeTeam', attributes: { exclude: ['id'] } },
         { model: Teams, as: 'awayTeam', attributes: { exclude: ['id'] } },
