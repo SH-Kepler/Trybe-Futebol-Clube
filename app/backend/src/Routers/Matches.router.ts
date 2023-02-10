a5bc687c18f0a0f11b8d5eb19731b625b7576f91
Sheifer Kepler <sheifer.kda@gmail.com>
2023-02-10 09:56:26 -0300

    feat: router match post

--- app/backend/src/Routers/Matches.router.ts
+++ app/backend/src/Routers/Matches.router.ts
@@ -1,8 +1,12 @@
 import { Router } from 'express';
+import ValidateToken from '../middlewares/validateToken';
 import MatchesController from '../controllers/Matches.controller';
+
+const validateToken = new ValidateToken();
 
 const router = Router();
 
 router.get('/', MatchesController.getAllMatches);
+router.post('/', validateToken.authorization, MatchesController.createNewMatch);
 
 export default router;
