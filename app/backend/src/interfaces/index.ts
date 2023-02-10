bf06681e5f458a7309a34d95a8ffdd63aa8f84fe
Sheifer Kepler <sheifer.kda@gmail.com>
2023-02-10 09:53:56 -0300

    feat: match interface

--- app/backend/src/interfaces/index.ts
+++ app/backend/src/interfaces/index.ts
@@ -2,3 +2,12 @@ export interface ILogin {
   email: string,
   password: string
 }
+
+export interface IMatch {
+  id?: number;
+  homeTeamId: number;
+  homeTeamGoals: number;
+  awayTeamId: number;
+  awayTeamGoals: number;
+  inProgress?: boolean;
+}
