88c566b9ab0fa8bc6b7e0221c962d3f97b72e77d
Sheifer Kepler <sheifer.kda@gmail.com>
2023-02-13 18:18:09 -0300

    feat: route get leaderboard/away

--- app/backend/src/Routers/Leaderboard.router.ts
+++ app/backend/src/Routers/Leaderboard.router.ts
@@ -4,5 +4,6 @@ import LeaderboardController from '../controllers/Leaderboard.controller';
 const router = Router();
 
 router.get('/home', LeaderboardController.getHomeTeamLeaderboard);
+router.get('/away', LeaderboardController.getAwayTeamLeaderboard);
 
 export default router;
