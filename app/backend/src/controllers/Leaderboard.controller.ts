6a96fd3986fc4b55d77438e5efdc553abdd3571d
Sheifer Kepler <sheifer.kda@gmail.com>
2023-02-13 18:17:55 -0300

    feat: controller get leaderboard/away

--- app/backend/src/controllers/Leaderboard.controller.ts
+++ app/backend/src/controllers/Leaderboard.controller.ts
@@ -3,7 +3,13 @@ import LeaderboardService from '../services/Leaderboard.service';
 
 export default class LeaderboardController {
   static async getHomeTeamLeaderboard(req: Request, res:Response) {
-    const { status, teams } = await LeaderboardService.getHomeTeamLeaderboard();
+    const { status, teams } = await LeaderboardService.getTeamLeaderboard('home');
+
+    return res.status(status).json(teams);
+  }
+
+  static async getAwayTeamLeaderboard(req: Request, res:Response) {
+    const { status, teams } = await LeaderboardService.getTeamLeaderboard('away');
 
     return res.status(status).json(teams);
   }
